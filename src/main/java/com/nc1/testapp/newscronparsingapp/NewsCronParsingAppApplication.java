f0eb59854e9ca31e433e54dae16e559863f1033e
Dmytro Lemeshchenko <dimmonishe@gmail.com>
2024-09-22 01:20:34 +0300

    The common structure for the application was created. Docker container properties for the MySQL database was created. So lets writing code!

---
+++ src/main/java/com/nc1/testapp/newscronparsingapp/NewsCronParsingAppApplication.java
@@ -0,0 +1,13 @@
+package com.nc1.testapp.newscronparsingapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewsCronParsingAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NewsCronParsingAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/nc1/testapp/newscronparsingapp/NewsCronParsingAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nc1.testapp.newscronparsingapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NewsCronParsingAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
